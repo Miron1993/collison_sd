c1352ff2de06335db06b4255d21fe186cc95946c
Miron1993 <miron.karim@gmail.com>
2024-05-27 10:36:03 +0530

    products migration added

---
+++ database/migrations/2024_05_27_045816_create_product_discounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_discounts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->string('code');
+            $table->double('discount')->default('00.00');
+            $table->string('discount_type')->default('percentage')->comment('value will be percentage, amount, etc');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_discounts');
+    }
+};
