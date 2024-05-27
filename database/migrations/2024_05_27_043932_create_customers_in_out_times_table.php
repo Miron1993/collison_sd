6b08d732d251f8cf2dd6b0159da0cc8c815c5388
Miron1993 <miron.karim@gmail.com>
2024-05-27 10:27:10 +0530

    customers migration added

---
+++ database/migrations/2024_05_27_043932_create_customers_in_out_times_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers_in_out_times', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->date('in_time');
+            $table->date('out_time');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers_in_out_times');
+    }
+};
